1c6cb46ffb216995baf06b9859fa89ee7c2198ff
Aleksander Biłka <aleksanderbilka@gmail.com>
2019-01-22 13:32:27 +0100

    Upgrade from base to base+ with type size,

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,14 +6,69 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            string name = args[0];
-            Console.WriteLine(name);
-            Console.WriteLine("Tralala Tralali!");
+            ValueType();
+            for (; ; ) // pusty nieskończony for wymagane 2 ;
+            {
+                Greeting();
+                Age();
+                ResetSettings();
+            }
+        }
 
-            //foreach (var item in args)
-            //{
-            //    Console.WriteLine(item);
-            //}
+        private static void ValueType()
+        {
+            int maxInt = int.MaxValue; // właściwość nie metoda () 
+            int minInt = int.MinValue;
+            long maxLong = long.MinValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("Max Int = " + maxInt);
+            Console.WriteLine("Min Int = " + minInt);
+            Console.WriteLine("Max long = " + maxLong);
+            Console.WriteLine("Min Long = " + minLong);
+        }
+
+        /// <summary>
+        /// reset ustawien Console
+        /// </summary>
+        private static void ResetSettings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor(); // lol ? metoda na reset koloru xD 
+        }
+
+        /// <summary>
+        /// Pobieramy wiek
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Podaj wiek: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age); //tryParse lepsze od Parse
+            if (age >= 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Pijesz wóde.");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Wprowadź odpowiedni wiek!");
+            }
+            else
+            {
+                Console.WriteLine("No to mleczki.");
+            }
+        }
+
+        /// <summary>
+        /// Pobieramy Imię
+        /// </summary>
+        private static void Greeting()
+        {
+            Console.Write("Wpisz imie: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
         }
     }
 }
