8f734b5a2f3ffdffbdf896236425f392a23e6a61
Aleksander Biłka <aleksanderbilka@gmail.com>
2019-01-24 11:36:12 +0100

    New Project "Diary" / New Class

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        // stan klasy (zmienne w klasie pola)
+        List<float> ratings = new List<float>(); // lista - kolekcja generyczna // 
+
+        // zachowania klasy (zazwyczaj metody)
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAvarage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count(); // count da ilość elementów kolekcji
+            return avg;
+        }
+
+        public float MaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float MinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary(); //Diary() - metoda konstruktor; Tworzenie instancji obiektu typu klasa diary 
+            //diary.AddRating(5); // niejawna konwersja bo nic nie tracimy
+            //diary.AddRating(8.5f); // double nie da się przekazać do float dlatego jest f żeby wiedział że to jest typ float
+            //diary.AddRating(4.7f);
+            //diary.AddRating(3.4f);
+
+            //float avg = diary.CalculateAvarage();
+            //float max = diary.MaxRating();
+            //float min = diary.MinRating();
+
+
+            for (; ;)
+            {
+                Console.WriteLine("Podaj ocene: ");
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating); 
+                if (rating == 11)
+                {
+                    break;
+                }
+                if (result && rating <= 10 && rating >= 0) 
+                {
+                    diary.AddRating(rating);
+                }
+                else
+                {
+                    Console.WriteLine("Błędna liczba!");
+                }
+                
+            }
+
+            Console.WriteLine("Średnia Twoich ocen to: " + diary.CalculateAvarage());
+            Console.WriteLine("Max: " + diary.MaxRating());
+            Console.WriteLine("Min: " + diary.MinRating());
+            Console.ReadKey();
+        }
+    }
+}
